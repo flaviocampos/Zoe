f5d30fd5679319b20e602d696016f6a8eff08557
flaviocampos <flaviorcampos15@gmail.com>
2024-05-28 22:53:18 -0300

    Ajuste na lentidão 3P.

--- src/pages/terceira-page.vue
+++ src/pages/terceira-page.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <VCard title="Sempre Alerta, mas vamos ver, 50 🙌">
+    <VCard title="Sempre Alerta, mas vamos ver, 5011 🙌">
       <VCardText>Esta é a Terceira Página</VCardText>
       <VCardText>
         Flávio, aparentemente é legal, mas é muito lento, tem que verificar se depois de publicado, melhora.
         Vamos evoluindo pois é preciso.
+        Agora com gt. Mas esta dureza a lentidão
       </VCardText>
     </VCard>
   </div>
