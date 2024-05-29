1ed08f3108fa7f28ba9fef95c836c9835a56e4de
flaviocampos <flaviorcampos15@gmail.com>
2024-05-28 23:10:57 -0300

    Pagina p4

---
+++ src/pages/quarta-page.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <VCard title="Sempre Alerta, mas vamos ver, 5011 🙌">
+      <VCardText>Esta é a Terceira Página</VCardText>
+      <VCardText>
+        Flávio, aparentemente é legal, mas é muito lento, tem que verificar se depois de publicado, melhora.
+        Vamos evoluindo pois é preciso.
+        Agora com gt. Mas esta dureza a lentidão
+      </VCardText>
+    </VCard>
+  </div>
+</template>
